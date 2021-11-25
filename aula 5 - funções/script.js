0df4e97b4242b800c32881321ff12e8767c58d8f
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-11-25 19:08:17 -0300

    Aula 5

---
+++ aula 5 - funções/script.js
@@ -0,0 +1,14 @@
+// exemplo de função
+function funcao() {
+    console.log("Tudo certo!!");
+}
+
+funcao();
+
+
+// função com parâmetros
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo);
+}
+
+mensagem('Olá', 'Aprendiz!!!');
